b4ed0fcfedd2bdae64708fe2fdeefa07136ede98
AndrewStoll <aws52b@mail.missouri.edu>
2017-11-17 10:59:26 -0600

    View Picker is fully functional

---
+++ Color Picker/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Color Picker
+//
+//  Created by Andrew W Stoll on 11/16/17.
+//  Copyright © 2017 Andrew W Stoll. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple)]
 
+    @IBOutlet weak var colorLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        colorLabel.text = colors[row].name
+        self.view.backgroundColor = colors[row].uiColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = colors[0].uiColor
+        colorLabel.text = colors[0].name
     }
 
     override func didReceiveMemoryWarning() {
